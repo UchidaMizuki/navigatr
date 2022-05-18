2954cd1087d7cfb34ca37f15503c84ae10d4e7dd
Uchida Mizuki <35052925+UchidaMizuki@users.noreply.github.com>
2022-05-18 23:56:48 +0900

    Update empty_item.R

--- R/empty_item.R
+++ R/empty_item.R
@@ -5,6 +5,10 @@ new_empty_item <- function(x = list(),
             class = c(class, "navigatr_empty_item"))
 }
 
+is_empty_item <- function(x) {
+  inherits(x, "navigatr_empty_item")
+}
+
 #' @export
 vec_ptype_abbr.navigatr_empty_item <- function(x) {
   "empty"
